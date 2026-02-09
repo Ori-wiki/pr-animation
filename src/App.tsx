a27ceff5e345571f45b55b46b00fe573524ec374
Denis <Ori21wiki@gmail.com>
2026-02-09 14:23:56 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className='max-w-360 w-full my-0 mx-auto min-h-full flex flex-col relative border-l border-r border-[#e2e5ec]'>
+      <div className='border-l border-[#e2e5ec] absolute left-1/2 w-px h-full'></div>
+      <div className=''></div>
+      <div className=''>Исторические даты</div>
+    </div>
+  );
+}
+
+export default App;
